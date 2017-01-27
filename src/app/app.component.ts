e7b95856bbb309c51db239edba8fb4caf3f69025
sjmg1982 <sjmg1982@gmail.com>
2017-01-27 19:43:53 +0100

    subida inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Http} from "@angular/http";
+
+const PEOPLE_API_URL="http://swapi.co/api/people/";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'app works!';
+
+  constructor(private  http:Http) {}
+
+  ngOnInit() {
+    this.http.get(PEOPLE_API_URL);
+  }
 }
